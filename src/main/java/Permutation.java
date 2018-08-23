5867ea327c62bc1055c3fbd6bda43eb96441e635
oocl-vito <hellozojian@gmail.com>
2018-08-23 13:58:24 +0800

    [cy vito] refactor call() implement input two chars

--- src/main/java/Permutation.java
+++ src/main/java/Permutation.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -6,6 +7,12 @@ import java.util.List;
  */
 public class Permutation {
 	public List<String> call(String input) {
+		if (input.length() == 2){
+			List<String> permutations = new ArrayList<>();
+			permutations.add(input);
+			permutations.add(input.substring(1,2)+input.substring(0,1));
+			return permutations;
+		}
 		return Arrays.asList(input);
 	}
 }
