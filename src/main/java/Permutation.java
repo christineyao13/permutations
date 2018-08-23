886df349342fdb146df4de523cc6b8332b96de57
oocl-vito <hellozojian@gmail.com>
2018-08-23 14:33:39 +0800

    [cy vito] refator call() input 3 chars

--- src/main/java/Permutation.java
+++ src/main/java/Permutation.java
@@ -15,14 +15,35 @@ public class Permutation {
 		}
 		if (input.length() == 3) {
 			List<String> permutations = new ArrayList<>();
-			permutations.add(input);
-			permutations.add(input.substring(0, 1) + input.substring(2, 3) + input.substring(1, 2));
-			permutations.add(input.substring(1, 2) + input.substring(0, 1) + input.substring(2, 3));
-			permutations.add(input.substring(1, 2) + input.substring(2, 3) + input.substring(0, 1));
-			permutations.add(input.substring(2, 3) + input.substring(0, 1) + input.substring(1, 2));
-			permutations.add(input.substring(2, 3) + input.substring(1, 2) + input.substring(0, 1));
+
+			for (int index = 0; index < input.length(); index++) {
+				List<String> subPermutations = call(removeOneChar(input, index));
+				permutations.add(input.charAt(index) + subPermutations.get(0));
+				permutations.add(input.charAt(index) + subPermutations.get(1));
+
+			}
+
+//
+//			List<String> subpermutations = call(removeOneChar(input, 0));
+//			permutations.add(input.substring(0, 1) + subpermutations.get(0));
+//			permutations.add(input.substring(0, 1) + subpermutations.get(1));
+//
+//			subpermutations = call(removeOneChar(input, 1));
+//			permutations.add(input.substring(1, 2) + subpermutations.get(0));
+//			permutations.add(input.substring(1, 2) + subpermutations.get(1));
+//
+//			subpermutations = call(removeOneChar(input, 2));
+//			permutations.add(input.substring(2, 3) + subpermutations.get(0));
+//			permutations.add(input.substring(2, 3) + subpermutations.get(1));
 			return permutations;
 		}
 		return Arrays.asList(input);
+	}
+
+	private String removeOneChar(String input,int index) {
+		if (index == 0){
+			return input.substring(1);
+		}
+		return input.substring(0, index) + input.substring(index+1);
 	}
 }
