f13d235187618a40e19c0649b71e12c1680aa9dc
oocl-vito <hellozojian@gmail.com>
2018-08-23 14:08:17 +0800

    [cy vito] refactor call when input three chars

--- src/main/java/Permutation.java
+++ src/main/java/Permutation.java
@@ -7,10 +7,20 @@ import java.util.List;
  */
 public class Permutation {
 	public List<String> call(String input) {
-		if (input.length() == 2){
+		if (input.length() == 2) {
 			List<String> permutations = new ArrayList<>();
 			permutations.add(input);
-			permutations.add(input.substring(1,2)+input.substring(0,1));
+			permutations.add(input.substring(1, 2) + input.substring(0, 1));
+			return permutations;
+		}
+		if (input.length() == 3) {
+			List<String> permutations = new ArrayList<>();
+			permutations.add(input);
+			permutations.add(input.substring(0, 1) + input.substring(2, 3) + input.substring(1, 2));
+			permutations.add(input.substring(1, 2) + input.substring(0, 1) + input.substring(2, 3));
+			permutations.add(input.substring(1, 2) + input.substring(2, 3) + input.substring(0, 1));
+			permutations.add(input.substring(2, 3) + input.substring(0, 1) + input.substring(1, 2));
+			permutations.add(input.substring(2, 3) + input.substring(1, 2) + input.substring(0, 1));
 			return permutations;
 		}
 		return Arrays.asList(input);
