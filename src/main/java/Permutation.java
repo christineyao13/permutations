d5440fe5f71b8b0f0e5c589bbb3ab0f4e09369c1
oocl-vito <hellozojian@gmail.com>
2018-08-23 15:18:36 +0800

    [cy vito] refactor call() when input more than 3 chars

--- src/main/java/Permutation.java
+++ src/main/java/Permutation.java
@@ -1,32 +1,27 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 /**
  * Created by Vito Zhuang on 8/23/2018.
  */
-public class Permutation {
-	public List<String> call(String input) {
-		if (input.length() == 2) {
-			List<String> permutations = new ArrayList<>();
-			permutations.add(input);
-			permutations.add(input.substring(1, 2) + input.substring(0, 1));
-			return permutations;
+class Permutation {
+	List<String> call(String input) {
+		if (input.length() == 1) {
+			return Collections.singletonList(input);
 		}
-		if (input.length() == 3) {
-			List<String> permutations = new ArrayList<>();
-
-			for (int index = 0; index < input.length(); index++) {
-				List<String> subPermutations = call(removeOneCharByIndex(input, index));
-				permutations.add(input.charAt(index) + subPermutations.get(0));
-				permutations.add(input.charAt(index) + subPermutations.get(1));
+		List<String> permutations = new ArrayList<>();
+		for (int i = 0; i < input.length(); i++) {
+			List<String> subPermutations = call(removeOneCharByIndex(input, i));
+			for (String subPermutation : subPermutations) {
+				permutations.add(input.charAt(i) + subPermutation);
 			}
-			return permutations;
 		}
-		return Collections.singletonList(input);
+		return permutations;
 	}
 
 	private String removeOneCharByIndex(String input, int index) {
-		return input.substring(0, index) + input.substring(index+1);
+		return input.substring(0, index) + input.substring(index + 1);
 	}
 }
